d6050670c31aa35e41f01c35ba498798a6089c84
John Dawes <jdawes@bluebitmedia.com>
2018-07-26 16:45:41 -0600

    Pricing section and animiation

--- main.js
+++ main.js
@@ -48,4 +48,19 @@ $(document).ready(function() {
       .siblings()
       .removeClass("active-item");
   });
+
+  // Pricing Card Animation
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+
+    if (position >= 4300) {
+      $(".card-1").addClass("moveFromLeft");
+      $(".card-2").addClass("moveFromBottom");
+      $(".card-3").addClass("moveFromRight");
+    } else {
+      $(".card-1").removeClass("moveFromLeft");
+      $(".card-2").removeClass("moveFromBottom");
+      $(".card-3").removeClass("moveFromRight");
+    }
+  });
 });
